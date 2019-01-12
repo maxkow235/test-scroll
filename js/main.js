6b62e5e456a9e6a1c20df9ba1315ed40cfb6712e
Maxym Kovalskiy <maxkov228@gmail.com>
2019-01-12 02:10:29 +0200

    Scroll fixed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
-
-
-
 $(document).ready(function() {
-$('#fullpage').fullpage({
+	$('#fullpage').fullpage({
 		//options here
 		scrollHorizontally: true,
-		slidesNavigation:false,
+		slidesNavigation: false,
 		scrollHorizontallyKey: 'YWx2YXJvdHJpZ28uY29tX01mU2MyTnliMnhzU0c5eWFYcHZiblJoYkd4NVNRcg==',
-		menu:".fullpage-nav",
+		menu: ".fullpage-nav",
 		scrollOverflow: true,
-		anchors:['page1','page2','page3','page4','page5','page6'],
-		afterLoad:function(origin,destination, direction) {
-		
-			if(destination.anchor == "page5") {
+
+		fitToSection: false,
+
+		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+		afterLoad: function(origin, destination, direction) {
+			let darkSections = ['page4', 'page5']
+			if (darkSections.indexOf(destination.anchor) !== -1) {
 
 				$('body').addClass('dark_theme')
 			} else {
 				$('body').removeClass('dark_theme')
 			}
+		},
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+			
+			$(`.submenu a[href^="#${anchorLink.anchor}"`).removeClass('active')
+			$(`.submenu a[href^="#${anchorLink.anchor}"`).eq(slideAnchor.index).addClass('active')
+			
+
+
 		}
 	});
 
 
+
 })
-	
